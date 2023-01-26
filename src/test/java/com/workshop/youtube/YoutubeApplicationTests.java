0880d5cc06a7d52e404ec349e7251484e16d8075
Andres Sepulveda <bodyweightforce@gmail.com>
2023-01-26 19:57:17 -0300

    Initial commit

---
+++ src/test/java/com/workshop/youtube/YoutubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.youtube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
